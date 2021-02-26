1f4c4a7efde68e09b2b13e58e1df9d0392b584e2
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-02-26 14:36:03 +0800

    feat(storage/api.go): init

--- storage/api.go
+++ storage/api.go
@@ -6,4 +6,5 @@ type API interface {
 	SaveSpanner(*trace.Spanner) error
 	LoadSpanner(spanId uint64) (*trace.Spanner, error)
 	LoadTracer(traceId string) (*trace.Tracer, error)
+	SaveTracer(*trace.Tracer) error
 }
