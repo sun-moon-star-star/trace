f284ff5f1f4ae148366d43b7edd2c4cb4b9a53b9
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-03-03 16:20:18 +0800

    feat(storage/api.go): DefaultAPI

--- storage/api.go
+++ storage/api.go
@@ -1,10 +1,17 @@
 package storage
 
-import "trace"
+import (
+	"trace"
+	"trace/storage/mysql"
+)
 
 type API interface {
 	LoadTracer(*trace.Tracer) error
 	SaveTracer(*trace.Tracer) error
 	LoadSpanner(*trace.Spanner) error
 	SaveSpanner(*trace.Spanner) error
+}
+
+func DefaultAPI() API {
+	return &mysql.Mysql{}
 }
